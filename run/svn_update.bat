c900ec6b79b0f3ce3cd860ea1838649bd89f8f57
ray820328 <even.wrok@gmail.com>
2020-03-12 10:48:48 +0800

    Update svn_update.bat

--- run/svn_update.bat
+++ run/svn_update.bat
@@ -1,4 +1,4 @@
 cd /d d:/work/jiratest
-svn cleanup --username admin --password admin --non-interactive
-svn update --username admin --password admin --non-interactive
+svn cleanup --username xxx --password xxx --non-interactive
+svn update --username xxx --password xxx --non-interactive
 exit
