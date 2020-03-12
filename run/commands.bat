8f6a9014464882bdf07195c58e901973d0b52ce7
ray820328 <even.wrok@gmail.com>
2020-03-12 10:49:12 +0800

    Update commands.bat

--- run/commands.bat
+++ run/commands.bat
@@ -1,9 +1,9 @@
 del /q "d:\packtemp\trunk\test003.txt"
-svn export --username admin --password admin --non-interactive --force -r 27 "https://lenovo-pc/svn/jiratest/trunk/yyy" "d:/packtemp/trunk/yyy"
-svn export --username admin --password admin --non-interactive --force -r 28 "https://lenovo-pc/svn/jiratest/trunk/xxx" "d:/packtemp/trunk/xxx"
-svn export --username admin --password admin --non-interactive --force -r 28 "https://lenovo-pc/svn/jiratest/trunk/xxx/xx.txt" "d:/packtemp/trunk/xxx/xx.txt"
-svn export --username admin --password admin --non-interactive --force -r 28 "https://lenovo-pc/svn/jiratest/trunk/yyy/xx - 副本.txt" "d:/packtemp/trunk/yyy/xx - 副本.txt"
+svn export --username admin --password xxx --non-interactive --force -r 27 "https://lenovo-pc/svn/jiratest/trunk/yyy" "d:/packtemp/trunk/yyy"
+svn export --username admin --password xxx --non-interactive --force -r 28 "https://lenovo-pc/svn/jiratest/trunk/xxx" "d:/packtemp/trunk/xxx"
+svn export --username admin --password xxx --non-interactive --force -r 28 "https://lenovo-pc/svn/jiratest/trunk/xxx/xx.txt" "d:/packtemp/trunk/xxx/xx.txt"
+svn export --username admin --password xxx --non-interactive --force -r 28 "https://lenovo-pc/svn/jiratest/trunk/yyy/xx - 副本.txt" "d:/packtemp/trunk/yyy/xx - 副本.txt"
 del /q "d:\packtemp\trunk\yyy\xx.txt"
-svn export --username admin --password admin --non-interactive --force -r 28 "https://lenovo-pc/svn/jiratest/trunk/yyy/zzz" "d:/packtemp/trunk/yyy/zzz"
+svn export --username admin --password xxx --non-interactive --force -r 28 "https://lenovo-pc/svn/jiratest/trunk/yyy/zzz" "d:/packtemp/trunk/yyy/zzz"
 pause
 exit
